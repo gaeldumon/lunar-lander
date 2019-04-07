d35ba5ef936ce10e6cf1af362346378bcd0d05cc
hyperdestru <slotuser@gmail.com>
2019-04-07 20:24:32 +0200

    Updated Map grid and make collision work (with lines and cols of ship)

--- main.lua
+++ main.lua
@@ -3,77 +3,40 @@ love.graphics.setDefaultFilter("nearest")
 
 function math.dist(x1,y1, x2,y2) return ((x2-x1)^2+(y2-y1)^2)^0.5 end
 
+function goodLanding(pvy, pAngle)
+	if math.abs(pvy) <= 0.6 and pAngle >= 265 and pAngle <= 275 then
+		return true
+	else
+		return false
+	end
+end
+
+
 function love.load()
+
 	GAME_WIDTH = love.graphics.getWidth()
 	GAME_HEIGHT = love.graphics.getHeight()
 
-	Map = {}
-	tilesheet = love.graphics.newImage('images/gc-tilesheet1.png')
-	TileTextures = {}
-	Map.TILE_HEIGHT = 32
-	Map.TILE_WIDTH = 32
-	Map.MAP_HEIGHT = 19
-	Map.MAP_WIDTH = 25
-	Map.Grid = { 
-		{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-		{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-		{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-		{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-		{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-		{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-		{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-		{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-		{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-		{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-		{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-		{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-		{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-		{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-		{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-		{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-		{ 53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53 },
-		{ 53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53 },
-		{ 53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53 }
-	}
-
-	TileTextures[0] = nil
-
-    --Reading/cuting each tilesheet tiles one by one : not related to the screen in any way
-    local nb_cols = tilesheet:getWidth() / Map.TILE_WIDTH
-    local nb_lines = tilesheet:getHeight() / Map.TILE_HEIGHT
-    local l,c
-    local id = 1
-    for l = 1, nb_lines do
-      for c = 1, nb_cols do
-        TileTextures[id] = love.graphics.newQuad(
-          (c - 1) * Map.TILE_WIDTH, 
-          (l - 1) * Map.TILE_HEIGHT, 
-          Map.TILE_WIDTH, 
-          Map.TILE_HEIGHT, 
-          tilesheet:getWidth(),
-          tilesheet:getHeight()
-        )
-        id = id + 1
-      end
-    end
-    ----
-
 	Lander = {}
-	Lander.x = 0
-	Lander.y = 0
+	Lander.x = GAME_WIDTH / 2
+	Lander.y = GAME_HEIGHT / 2
 	Lander.vx = 0
 	Lander.vy = 0
 	Lander.angle = 270
 	Lander.speed = 3
 	Lander.vmax = 2
-	Lander.fuel = 200
+	Lander.fuel = 100
 	Lander.engine_on = false
+	Lander.crash = false
+
 	Lander.img = love.graphics.newImage("images/ship.png")
 	Lander.width = Lander.img:getWidth()
 	Lander.height = Lander.img:getHeight()
+
 	Lander.img_engine = love.graphics.newImage("images/engine.png")
 	Lander.engine_width = Lander.img_engine:getWidth()
 	Lander.engine_height = Lander.img_engine:getHeight()
+
 	Lander.sound = love.audio.newSource('sounds/spaceship.ogg', 'static')
 	Lander.sound:isLooping(true)
 
@@ -82,8 +45,58 @@ function love.load()
 	Stars = {}
 	Stars.number = 100
 
-	Lander.x = GAME_WIDTH / 2
-	Lander.y = GAME_HEIGHT / 2
+	Map = {}
+	Map.tilesheet = love.graphics.newImage('images/gc-tilesheet1.png')
+	Map.TileTextures = {}
+
+	Map.Grid = { 
+					{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+					{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+					{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+					{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+					{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+					{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+					{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+					{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+					{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0	},
+					{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0	},
+					{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0	},
+					{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0	},
+					{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,53 },
+					{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,53 },
+					{ 53,53,0,0,0,0,0,0,0,53,53,53,53,0,0,0,0,0,0,0,0,0,0,53,53 },
+					{ 53,53,53,0,0,0,0,53,53,53,53,53,53,53,0,0,0,0,0,0,0,0,0,53,53 },
+					{ 53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53 },
+					{ 53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53 },
+					{ 53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53,53 }
+	}
+
+	Map.TILE_HEIGHT = 32
+	Map.TILE_WIDTH = 32
+	Map.MAP_HEIGHT = 19
+	Map.MAP_WIDTH = 25
+
+	Map.TileTextures[0] = nil
+
+    --Reading/cuting each Map.tilesheet tiles one by one : not related to the screen in any way
+    local nb_cols = Map.tilesheet:getWidth() / Map.TILE_WIDTH
+    local nb_lines = Map.tilesheet:getHeight() / Map.TILE_HEIGHT
+    local l,c
+    local id = 1
+    for l = 1, nb_lines do
+      for c = 1, nb_cols do
+        Map.TileTextures[id] = love.graphics.newQuad(
+          (c - 1) * Map.TILE_WIDTH, 
+          (l - 1) * Map.TILE_HEIGHT, 
+          Map.TILE_WIDTH, 
+          Map.TILE_HEIGHT, 
+          Map.tilesheet:getWidth(),
+          Map.tilesheet:getHeight()
+        )
+        id = id + 1
+      end
+    end
+    ----
 
 	----Creating random Stars coordinates with a margin of 5px
 	for i = 1, Stars.number do
@@ -97,8 +110,8 @@ function love.load()
 end
 
 
-
 function love.update(dt)
+
 	--Simulate the gravity pull of the moon : the lander is sort of attracted to the bottom because we increment its vertical velocity
 	--It's an incrementation so it's like an acceleration : the vertical velocity is not constant (that'd have been : Lander.vy = gravity)
 	Lander.vy = Lander.vy + (gravity * dt)
@@ -124,7 +137,7 @@ function love.update(dt)
 		if Lander.angle < 0 then Lander.angle = 360 end
 	end
 
-	if love.keyboard.isDown('up') then
+	if love.keyboard.isDown('up') and Lander.fuel > 0 then
 		----Movement equations
 		local angle_rad = math.rad(Lander.angle)
 		local force_x = math.cos(angle_rad) * (Lander.speed * dt)
@@ -135,6 +148,7 @@ function love.update(dt)
 		----
 
 		Lander.engine_on = true
+		Lander.fuel = Lander.fuel - 1
 		Lander.sound:play()
 	else
 		Lander.engine_on = false
@@ -142,42 +156,61 @@ function love.update(dt)
 	end
 	----
 
-	if Lander.y > GAME_HEIGHT - Map.TILE_HEIGHT * 3 then
-		Lander.vy = 0
-		Lander.vx = 0
-		gravity = 0
-		Lander.y = GAME_HEIGHT - Map.TILE_HEIGHT * 3 
+	----SHIP GROUND COLLISION
+	do
+		local c = math.floor(Lander.x / Map.TILE_WIDTH) + 1
+		local l = math.floor((Lander.y + Lander.height / 2) / Map.TILE_HEIGHT) + 1
+		if Map.Grid[l][c] == 53 then
+			if goodLanding(Lander.vx, Lander.vy, Lander.angle) == true then
+				Lander.vx = 0
+				Lander.vy = 0
+				gravity = 0
+				Lander.crash = false
+			else
+				Lander.crash = true
+				Lander.vx = 0
+				Lander.vy = 0
+				gravity = 0
+			end
+		else
+			gravity = 0.6
+		end
 	end
+	----
+
 end
 
 
-
 function love.draw()
-	----Drawing the actual textures "cut" off the tilesheet in Game.Load()
+
+	----Drawing the actual textures "cut" off the Map.tilesheet in Game.Load()
     local c, l
     for l = 1, Map.MAP_HEIGHT do
       for c = 1, Map.MAP_WIDTH do
         local id = Map.Grid[l][c]
-        local texQuad = TileTextures[id]
+        local texQuad = Map.TileTextures[id]
         if texQuad ~= nil then
             local x = (c - 1) * Map.TILE_WIDTH
             local y = (l - 1) * Map.TILE_HEIGHT
-            love.graphics.draw(tilesheet, texQuad, x, y)
+            love.graphics.draw(Map.tilesheet, texQuad, x, y)
         end
       end
     end
     ----
-	--Drawing the Stars. Really handy new points methods : just give a metatable (2 dimensions) with x and y coordinates to it
+
+	--DRAW STARS
 	love.graphics.points(Stars)
 
-	--Drawing the ship
+	--DRAW THE SHIP
 	love.graphics.draw(Lander.img, Lander.x, Lander.y, math.rad(Lander.angle), 1, 1, Lander.width / 2, Lander.height / 2)
 
-	--Drawing the exhaust of the engine
+	----DRAW ENGINE EXHAUST
 	if Lander.engine_on == true then
 		love.graphics.draw(Lander.img_engine, Lander.x, Lander.y, math.rad(Lander.angle), 1, 1, Lander.engine_width / 2, Lander.engine_height / 2)
 	end
+	----
 
+	----PRINTING LANDER DATA
 	local abs_vy = math.abs(Lander.vy)
 	local abs_vx = math.abs(Lander.vx)
 	local str_data = ""
@@ -187,4 +220,6 @@ function love.draw()
 	str_data = str_data .. "FUEL : " .. tostring(Lander.fuel) .. "\n"
 
 	love.graphics.print(str_data, 20, 20, 0, 1.1, 1.1)
+	----
+
 end
